727d565e4f2535e2500d7e6d59210379d7c173ea
Mariano Favre <marianofavre79@yahoo.com.ar>
2023-06-27 19:11:55 -0300

    rename interfaces, descomentar dependencia del conector de MySql en el pom.xml, modificación y comentarios del application.properties, prueba de archivos .sql

---
+++ src/main/resources/data1.sql
@@ -0,0 +1,5 @@
+INSERT INTO usuario(telefono, nombre_usuario, tipo_usuario)
+VALUES (111111,'Mariano','CLIENTE'),
+       (222222,'Ignacio', 'AGENTE_DE_VENTAS'),
+       (333333,'Martin', 'ADMINISTRADOR');
+
