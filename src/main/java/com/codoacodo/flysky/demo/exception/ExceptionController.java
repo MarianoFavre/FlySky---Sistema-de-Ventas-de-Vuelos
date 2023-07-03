0a06df14e7d51fd58d8d7caff920e8ba1d39fe55
Mariano Favre <marianofavre79@yahoo.com.ar>
2023-07-03 18:46:54 -0300

    Agregar excepcion NoSuchElementException y probar.

--- src/main/java/com/codoacodo/flysky/demo/exception/ExceptionController.java
+++ src/main/java/com/codoacodo/flysky/demo/exception/ExceptionController.java
@@ -6,11 +6,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.NoSuchElementException;
+
 @ControllerAdvice
 public class ExceptionController {
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<?> notFoundException(NotFoundException e){
+    public ResponseEntity<?> notFound(NotFoundException e){
         ExceptionDto exceptionDto = new ExceptionDto(404, e.getMessage());
         return new ResponseEntity<>(exceptionDto, HttpStatus.NOT_FOUND);
     }
@@ -20,6 +22,14 @@ public class ExceptionController {
         ExceptionDto exceptionDto = new ExceptionDto(401, e.getMessage());
         return new ResponseEntity<>(exceptionDto, HttpStatus.UNAUTHORIZED);
     }
+
+    @ExceptionHandler(.class)
+    public ResponseEntity<?> NoSuchElemen(NoSuchElementException e){
+        ExceptionDto exceptionDto = new ExceptionDto(500, e.getMessage()
+                .replace("No value present", "Usuario no registrado."));
+        return new ResponseEntity<>(exceptionDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 
 
 }
